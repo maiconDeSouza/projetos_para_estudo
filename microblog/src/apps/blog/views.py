aa07ca3beed095ff2ebe123a90108ca931cc5fc3
Maicon Souza <maicon_souza09@hotmail.com>
2025-07-18 21:41:23 -0300

    18-07-25 21:41

--- microblog/src/apps/blog/views.py
+++ microblog/src/apps/blog/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
 from django.views import View
+
+from .models import Post
 
 
 # Create your views here.
 class PostList(View):
     def get(self, request):
-        return HttpResponse('Post')
+        posts = Post.objects.all()
+        return render(request, 'blog/pages/home.html', {'posts': posts})
