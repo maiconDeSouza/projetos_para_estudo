7fdf60784dfc442db9cbdcef25e2a5c2fd2241d4
Maicon Souza <maicon_souza09@hotmail.com>
2025-09-25 23:45:57 -0300

    25-09-25 23:45

--- ultra_learning/apps/core/views.py
+++ ultra_learning/apps/core/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
 
 from .models import Project
 
 
 class Index(View):
     def get(self, request):
-        projects = Project.objects.filter(owner=request.user)
-
+        form = AuthenticationForm()
         context = {
-            'projects': projects,
+            'form': form,
         }
 
         return render(request, 'pages/index.html', context)
+
+
+# projects = Project.objects.filter(owner=request.user)
