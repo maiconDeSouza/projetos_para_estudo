acdcf38d6160b696fce1b3a1a9b27c37ea767646
Maicon Souza <maicon_souza09@hotmail.com>
2025-10-01 22:02:21 -0300

    01-09-25 22:02

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
 from .models import Question
 
 
 # Create your views here.
+class Initial(View):
+    def get(self, request):
+        return redirect('polls:index')
+
+
 class Index(View):
     def get(self, request):
         questions = Question.objects.all()
--- djangotutorial/setup/urls.py
+++ djangotutorial/setup/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from polls import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.Initial.as_view(), name='initial'),
     path('polls/', include('polls.urls', namespace='polls')),
 ]
