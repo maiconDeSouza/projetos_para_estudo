85af055aed84174cbdbf3c6d7391a8f61f9fd455
Maicon Souza <maicon_souza09@hotmail.com>
2025-10-20 18:51:37 -0300

    20-10-25 18:51

--- landing_page_larissa_kich/src/js/script.js
+++ landing_page_larissa_kich/src/js/script.js
@@ -1,21 +1,13 @@
 const buttonMobile = document.querySelector('.btn-mobile')
+const navbarMobile = document.querySelector('#id-navbar-mobile')
+const icon = buttonMobile.querySelector('i')
+
 
 buttonMobile.addEventListener('click', e => {
-    const navbarMobile = document.querySelector('#id-navbar-mobile')
-    const icon = buttonMobile.querySelector('i')
-
-    const classIcon = icon.classList.value
-
-
-    if(classIcon === 'fa-solid fa-bars'){
-        icon.classList.replace('fa-bars', 'fa-xmark')
-    }
-
-    if(classIcon === 'fa-solid fa-xmark'){
-        icon.classList.replace('fa-xmark', 'fa-bars')
-    }
-
     navbarMobile.classList.toggle('on')
+    const isNavbarOpen = navbarMobile.classList.contains('on')
 
+    icon.classList.toggle('fa-bars', !isNavbarOpen)
+    icon.classList.toggle('fa-xmark', isNavbarOpen)
 })
 
