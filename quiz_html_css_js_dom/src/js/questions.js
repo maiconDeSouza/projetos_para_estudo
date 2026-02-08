2ed8a84ce49b4db19b1569004d57f32e1a5804be
Maicon Souza <maicon_souza09@hotmail.com>
2026-02-08 11:36:22 -0300

    08-02-26 11:36

---
+++ quiz_html_css_js_dom/src/js/questions.js
@@ -0,0 +1,123 @@
+// questions.js
+export const questions = [
+  {
+    id: 'c1f3a9b0',
+    question: 'Qual é a capital do Brasil?',
+    options: ['Brasília', 'Rio de Janeiro', 'São Paulo', 'Salvador'],
+    answer: 'Brasília'
+  },
+  {
+    id: 'd2a4e7c1',
+    question: 'Qual é o maior planeta do Sistema Solar?',
+    options: ['Marte', 'Terra', 'Saturno', 'Júpiter'],
+    answer: 'Júpiter'
+  },
+  {
+    id: 'e3b5f2a4',
+    question: 'Quem escreveu "Dom Casmurro"?',
+    options: ['José de Alencar', 'Machado de Assis', 'Clarice Lispector', 'Jorge Amado'],
+    answer: 'Machado de Assis'
+  },
+  {
+    id: 'f4c6a3d5',
+    question: 'Em que ano o Brasil declarou sua independência?',
+    options: ['1808', '1822', '1889', '1910'],
+    answer: '1822'
+  },
+  {
+    id: 'a5d7b4e6',
+    question: 'Qual elemento químico tem o símbolo O?',
+    options: ['Oxigênio', 'Ouro', 'Prata', 'Níquel'],
+    answer: 'Oxigênio'
+  },
+  {
+    id: 'b6e8c5f7',
+    question: 'Qual a língua mais falada no mundo por número de falantes nativos?',
+    options: ['Inglês', 'Espanhol', 'Chinês Mandarim', 'Hindi'],
+    answer: 'Chinês Mandarim'
+  },
+  {
+    id: 'c7f9d6a8',
+    question: 'Qual é o maior oceano da Terra?',
+    options: ['Atlântico', 'Índico', 'Ártico', 'Pacífico'],
+    answer: 'Pacífico'
+  },
+  {
+    id: 'd8a0e7b9',
+    question: 'Qual festa é tradicionalmente celebrada com desfiles, samba e fantasias no Brasil?',
+    options: ['São João', 'Carnaval', 'Festa Junina', 'Oktoberfest'],
+    answer: 'Carnaval'
+  },
+  {
+    id: 'e9b1f8c0',
+    question: 'A quem é popularmente atribuída a invenção da lâmpada incandescente?',
+    options: ['Nikola Tesla', 'Thomas Edison', 'Alexander Graham Bell', 'Galileu Galilei'],
+    answer: 'Thomas Edison'
+  },
+  {
+    id: 'f0c2a9d1',
+    question: 'Qual é a fórmula química da água?',
+    options: ['CO2', 'H2', 'H2O', 'O2'],
+    answer: 'H2O'
+  },
+  {
+    id: 'a1d3b0e2',
+    question: 'Qual é a capital de Portugal?',
+    options: ['Lisboa', 'Porto', 'Coimbra', 'Faro'],
+    answer: 'Lisboa'
+  },
+  {
+    id: 'b2e4c1f3',
+    question: 'Qual o símbolo químico do ouro?',
+    options: ['Ag', 'Au', 'Fe', 'Pb'],
+    answer: 'Au'
+  },
+  {
+    id: 'c3f5d2a4',
+    question: 'Quem foi o primeiro homem a pisar na Lua?',
+    options: ['Yuri Gagarin', 'Buzz Aldrin', 'Michael Collins', 'Neil Armstrong'],
+    answer: 'Neil Armstrong'
+  },
+  {
+    id: 'd4a6e3b5',
+    question: 'Qual é a moeda do Japão?',
+    options: ['Won', 'Dólar', 'Iene', 'Euro'],
+    answer: 'Iene'
+  },
+  {
+    id: 'e5b7f4c6',
+    question: 'Quem pintou a Mona Lisa?',
+    options: ['Vincent van Gogh', 'Pablo Picasso', 'Michelangelo', 'Leonardo da Vinci'],
+    answer: 'Leonardo da Vinci'
+  },
+  {
+    id: 'f6c8a5d7',
+    question: 'Qual valor aproximado da velocidade da luz no vácuo?',
+    options: ['3.000 km/s', '30.000 km/s', '300.000 km/s', '3.000.000 km/s'],
+    answer: '300.000 km/s'
+  },
+  {
+    id: 'a7d9b6e8',
+    question: 'Qual é o menor número primo?',
+    options: ['2', '1', '3', '0'],
+    answer: '2'
+  },
+  {
+    id: 'b8e0c7f9',
+    question: 'Qual é o maior país em área do mundo?',
+    options: ['Canadá', 'China', 'Estados Unidos', 'Rússia'],
+    answer: 'Rússia'
+  },
+  {
+    id: 'c9f1d8a0',
+    question: 'Em que ano terminou a Segunda Guerra Mundial?',
+    options: ['1939', '1942', '1945', '1950'],
+    answer: '1945'
+  },
+  {
+    id: 'd0a2e9b1',
+    question: 'Quem é o autor da Odisseia (obras atribuídas)?',
+    options: ['Sófocles', 'Homero', 'Platão', 'Aristóteles'],
+    answer: 'Homero'
+  }
+]
