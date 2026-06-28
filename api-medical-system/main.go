a6b6d430a5cb73eeaf724974e002e05e8fac9ff3
Maicon Souza <maicon_souza09@hotmail.com>
2026-06-28 17:42:07 -0300

    28-06-26 17:42

--- api-medical-system/main.go
+++ api-medical-system/main.go
@@ -2,23 +2,68 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 )
 
-func main() {
-	layout := "2006-01-02 15:04"
-	data := time.Now().Local().Format(layout)
+type FormatData struct {
+	day     string
+	weekday string
+	month   string
+	year    string
+}
 
-	hr, err := time.Parse(layout, data)
-	if err != nil {
-		fmt.Println("Erro aoo converter:", err)
-		return
+func traduzirDia(d time.Weekday) string {
+	dias := map[time.Weekday]string{
+		time.Monday:    "Segunda-feira",
+		time.Tuesday:   "Terça-feira",
+		time.Wednesday: "Quarta-feira",
+		time.Thursday:  "Quinta-feira",
+		time.Friday:    "Sexta-feira",
+	}
+	return dias[d]
+}
+
+func main() {
+	// layout := "2006-01-02 15:04"
+	// day := time.Now().Local().Format(layout)
+
+	// hr, err := time.Parse(layout, data)
+	// if err != nil {
+	// 	fmt.Println("Erro aoo converter:", err)
+	// 	return
+	// }
+
+	// fmt.Println(hr)
+
+	// hj := time.Now()
+
+	// agendamento := hj.AddDate(0, 0, 30)
+	// fmt.Println(agendamento)
+
+	// layout := "2006-01-02 15:04"
+	day := time.Now()
+
+	days := []*FormatData{}
+
+	for i := 0; i <= 30; i++ {
+		ag := day.AddDate(0, 0, i)
+
+		if ag.Weekday() == time.Saturday || ag.Weekday() == time.Sunday {
+			continue
+		}
+
+		newData := FormatData{
+			day:     strconv.Itoa(ag.Day()),
+			weekday: traduzirDia(ag.Weekday()),
+			month:   ag.Month().String(),
+			year:    strconv.Itoa(ag.Year()),
+		}
+
+		days = append(days, &newData)
 	}
 
-	fmt.Println(hr)
-
-	hj := time.Now()
-
-	agendamento := hj.AddDate(0, 0, 30)
-	fmt.Println(agendamento.Month())
+	for _, v := range days {
+		fmt.Println(*v)
+	}
 }
