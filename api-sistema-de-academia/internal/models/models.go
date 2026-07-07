f5c40bf9b6b51d02c562d42c1c86faca78f4d910
Maicon Souza <maicon_souza09@hotmail.com>
2026-07-06 21:49:55 -0300

    06-07-26 21:49

--- api-sistema-de-academia/internal/models/models.go
+++ api-sistema-de-academia/internal/models/models.go
@@ -1,1 +1,41 @@
 package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Category string
+
+const (
+	Basic   Category = "Basico"
+	Premium Category = "Prêmio"
+)
+
+type ADM struct {
+	gorm.Model
+	NickName string
+	Password string
+}
+
+type User struct {
+	gorm.Model
+	NickName string
+	Password string
+	Category Category
+}
+
+type Gym struct {
+	gorm.Model
+	Name     string
+	Category Category
+}
+
+type CheckInCheckOut struct {
+	ID       uint
+	User     User
+	Gym      Gym
+	Checkin  time.Time
+	checkout time.Time
+}
