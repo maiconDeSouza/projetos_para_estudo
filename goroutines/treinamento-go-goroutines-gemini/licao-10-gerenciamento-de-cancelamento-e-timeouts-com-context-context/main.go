9c9d273c9d7ff2c2c0c397d01be6e5d0151da288
Maicon Souza <maicon_souza09@hotmail.com>
2026-07-29 06:33:09 -0300

    29-07-26 06:32

--- goroutines/treinamento-go-goroutines-gemini/licao-10-gerenciamento-de-cancelamento-e-timeouts-com-context-context/main.go
+++ goroutines/treinamento-go-goroutines-gemini/licao-10-gerenciamento-de-cancelamento-e-timeouts-com-context-context/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
+func buscarPrecoProduto(ctx context.Context, produto string) (string, error) {
+	ch := make(chan string, 1)
+
+	go func() {
+		time.Sleep(time.Duration(2) * time.Second)
+		ch <- fmt.Sprintf("Produto [%s]: R$ 99,00", produto)
+	}()
+
+	select {
+	case msg := <-ch:
+		return msg, nil
+	case <-ctx.Done():
+		return "", ctx.Err()
+	}
+}
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(rand.Intn(3)+1)*time.Second)
+	defer cancel()
+
+	msg, err := buscarPrecoProduto(ctx, "Notebook")
+	if err != nil {
+		fmt.Println("⏱️ Falha: operação cancelada por timeout!", err)
+		return
+	}
+
+	fmt.Println("✅ Sucesso:", msg)
 }
