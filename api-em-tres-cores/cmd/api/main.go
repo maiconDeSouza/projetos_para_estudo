23585e73f8e53788d99be10394bfc3b3ff6809b4
Maicon Souza <maicon_souza09@hotmail.com>
2026-08-23 17:32:09 -0300

    23-08-26 17:32

--- api-em-tres-cores/cmd/api/main.go
+++ api-em-tres-cores/cmd/api/main.go
@@ -3,25 +3,18 @@ package main
 import (
 	"api-em-tres-cores/internal/handler"
 	"api-em-tres-cores/internal/repository"
+	"api-em-tres-cores/internal/router"
 	"fmt"
 	"log"
 	"net/http"
 )
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome a API em três cores 🇾🇪")
-}
 
 const PORT = 2005
 
 func main() {
 	repo := repository.NewInMemoryRepository()
 	playerHandler := handler.NewPlayerHandler(repo)
-
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("GET /", homeHandler)
-	mux.HandleFunc("GET /players", playerHandler.GetPlayers)
+	mux := router.SetupRoutes(playerHandler)
 
 	fmt.Printf("Servidor rodando na porta: %d ... Bora Tricolor ⚽", PORT)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", PORT), mux))
---
+++ api-em-tres-cores/internal/router/router.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"api-em-tres-cores/internal/handler"
+	"fmt"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome a API em três cores 🇾🇪")
+}
+
+func SetupRoutes(playerHandler *handler.PlayerHandler) *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /api/v1", homeHandler)
+	mux.HandleFunc("GET /api/v1/players", playerHandler.GetPlayers)
+
+	return mux
+}
